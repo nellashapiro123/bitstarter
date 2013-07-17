52419fd2742512a93f0b45f51e1e884d52f047a8
Ubuntu <ubuntu@ip-172-31-33-51.us-west-2.compute.internal>
2013-07-17 08:21:44 +0000

    Added buffer

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buffer = fs.readFileSync("index.html");
+var helloString = buffer.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
+  response.send(helloString);
+}
+port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
